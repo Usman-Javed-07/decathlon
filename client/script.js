edf0b38612110456aba22fbf11935f495a27be57
Usman Javed <usmanjaved0816@gmail.com>
2026-01-08 20:52:05 +0500

    work on navbar and categories

---
+++ client/script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll(".links ul li a");
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    links.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
